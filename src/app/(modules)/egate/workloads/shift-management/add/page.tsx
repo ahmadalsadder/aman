e6babea6fd704066c0e1e6128471a0ee37e36ceb
Ahmad alsader <alsadder.ahmad91@gmail.com>
2025-07-26 06:07:03 +0000

    the egate module dose not have add, edit  shift pages

---
+++ src/app/(modules)/egate/workloads/shift-management/add/page.tsx
@@ -0,0 +1,6 @@
+
+'use client';
+
+// This page reuses the Airport module's component since the functionality is identical.
+import AddShiftPage from '@/app/(modules)/airport/workloads/shift-management/add/page';
+export default AddShiftPage;
---
+++ src/app/(modules)/egate/workloads/shift-management/edit/[id]/page.tsx
@@ -0,0 +1,6 @@
+
+'use client';
+
+// This page reuses the Airport module's component since the functionality is identical.
+import EditShiftPage from '@/app/(modules)/airport/workloads/shift-management/edit/[id]/page';
+export default EditShiftPage;
